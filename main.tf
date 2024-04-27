0cd2008639db2c629b52a483bdc14179ac73c552
fabianwbrown <108426627+fabianwbrown@users.noreply.github.com>
2024-04-26 22:56:16 -0400

    update

--- main.tf
+++ main.tf
@@ -9,4 +9,36 @@ terraform {
 
 provider "google" {
   # Configuration options
+  region = "us-east-1"
+
+}
+
+resource "google_storage_bucket" "bucket1" {
+  name          = "mentis_bucket83"
+  location      = "us-east-1"
+  force_destroy = true
+
+}
+
+
+resource "google_compute_network" "auto-vpc-83" {
+  name = "auto-vpc-83"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "sub-sg" {
+  name ="sub-sg83"
+  network = google_compute_network.auto-vpc-83.id
+  ip_cidr_range = "10.72.1.0/24"
+  region = "us-east-1"
+}
+
+
+#resource "google_compute_network" "custom-vpc-tf" {
+  #name = "custom-vpc-tf"
+ #auto_create_subnetworks = false
+#}
+
+output "auto" {
+  value = google_compute_network.auto-vpc-83.id
 }
